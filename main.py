ce6cf1091498d58c13f98f887e50b870cd3e318e
Oliver Scotten <revi99@me.com>
2022-05-17 22:44:22 +0000

    Testing / Progress

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from src import *
+import CloudFlare
 
 if __name__ == "__main__":
-    pass
+    wan = Network.Local.getWAN()
+    print(wan)
+
+    c = Config()
+    c.open("data/config.csv")
+    key = c.get("cloudflare", "api_key")
+    print(key)
+
+    rem = Network.Remote(key)
+    id = rem.getID()
+    print(id)
+
+    # cf = CloudFlare.CloudFlare(token=key)
+    # zones = cf.zones.get()
+    # for zone in zones:
+    #     zone_id = zone['id']
+    #     zone_name = zone['name']
+    #     print("zone_id=%s zone_name=%s" % (zone_id, zone_name))
