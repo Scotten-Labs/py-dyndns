71fa1149d9099edb15a4190679bae523f6dea05d
Oliver Scotten <revi99@me.com>
2022-05-17 22:43:55 +0000

    Config

---
+++ src/Config.py
@@ -0,0 +1,23 @@
+from csv import DictReader
+
+class Config:
+
+    def __init__(self):
+        self.data: list = None
+
+    def open(self, path: str) -> bool:
+        try:
+            with open(path) as file:
+                reader = DictReader(file)
+                self.data = list(reader)
+            return True
+        except FileNotFoundError:
+            raise FileNotFoundError
+
+    def get(self, service: str, value: str) -> str | bool | int | float:
+        try:
+            for i in self.data:
+                if i["service"] == service:
+                    return i[value]
+        except TypeError:
+            return None
