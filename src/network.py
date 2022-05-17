4e4888b67ffdca9aab7b4cd7ca2a098151a44aed
Oliver Scotten <revi99@me.com>
2022-05-17 22:44:05 +0000

    Network Tools

--- src/network.py
+++ src/network.py
@@ -1,5 +1,19 @@
-import urllib.request
+from xmlrpc.client import ResponseError
+import requests
+import CloudFlare
 
-def getWAN():
-    wan = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-    return wan
+class Local:
+
+    def getWAN():
+        wan = requests.get("https://api.ipify.org/").text
+        return wan
+
+class Remote(CloudFlare.CloudFlare):
+
+    def __init__(self, key: str):
+        super().__init__(token=key)
+        zones = self.zones.get()
+        self.ID = {z['name']:z['id'] for z in zones}
+
+    def getID(self) -> dict:
+        return self.ID
