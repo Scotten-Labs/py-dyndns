f1553d0be27d431480d31cc22c712c14f6ba1d62
Oliver Scotten <revi99@me.com>
2022-05-17 19:20:49 -0500

    Added Docstrings

--- src/Config.py
+++ src/Config.py
@@ -6,6 +6,17 @@ class Config:
         self.data: list = None
 
     def open(self, path: str) -> bool:
+        """Opens config file. Formatted as a CSV.
+
+        Args:
+            path (str): Path to File
+
+        Raises:
+            FileNotFoundError: File not Found
+
+        Returns:
+            bool: True when Opened
+        """
         try:
             with open(path) as file:
                 reader = DictReader(file)
@@ -15,6 +26,15 @@ class Config:
             raise FileNotFoundError("File not found at '" + path + "'.")
 
     def get(self, service: str, value: str) -> str | bool | int | float:
+        """Returns the key pair from the config file.
+
+        Args:
+            service (str): Service Name
+            value (str): Key Value
+
+        Returns:
+            str | bool | int | float: Key Value
+        """
         try:
             for i in self.data:
                 if i["service"] == service:
