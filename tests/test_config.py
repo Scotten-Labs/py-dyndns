65e64b8092e7cfb314437d293ff10e006acc0285
Oliver Scotten <revi99@me.com>
2022-05-19 09:19:16 +0000

    New Tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,4 @@
+import pytest
 from src import config
 
 def test_open():
@@ -9,5 +10,15 @@ def test_get():
     path = "data/config_template.csv"
     c = config.Config()
     c.open(path)
-    key = c.get("cloudflare", "api_key")
-    assert key == "<api_key>"
+    key = c.get("cloudflare", "api_token")
+    assert key == "<api_token>"
+    
+def test_get_error():
+    c = config.Config()
+    with pytest.raises(TypeError):
+        c.get("cloudflare", "api_token")
+        
+def test_open_error():
+    c = config.Config()
+    with pytest.raises(FileNotFoundError):
+        c.open("fake/path/to/config/file.csv")
