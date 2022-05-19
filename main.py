142f38ff2eeb333a5deab7c387cf8bfd3e6708e2
Oliver Scotten <revi99@me.com>
2022-05-19 23:43:40 +0000

    Main Run Loop

--- main.py
+++ main.py
@@ -1,60 +1,28 @@
-import csv
 from src import *
-import requests
+import time
 
 if __name__ == "__main__":
-    wan = network.Local.getWAN()
-    print(wan)
-
     c = Config()
     c.open("data/config.csv")
     token = c.get("cloudflare", "api_token")
     domain = c.get("cloudflare", "domain")
 
-    rem = network.Remote(token, domain)
-    zoneid = rem._zoneID
-    print(zoneid)
+    cf = network.Remote(token, domain)
+    cf.updateLocalRecords()
     
-    r = rem.updateLocalRecords()
+    records = ["test", "test2"]
     
-    print(r)
     
-    for i in rem.getRecords():
-        print(i["name"])
-
-    # x = requests.get(url=f"https://api.cloudflare.com/client/v4/zones/{zoneid}/dns_records",
-    #                  headers={"Authorization": f"Bearer {token}", "Content-Type": "application/json"})
-    # response = x.json()
-    # print(type(response))
-    
-    # fnames = set()
-    
-    # print(response)
-    
-    # for o in response:
-    #     fnames.add(o)
-    
-    # with open("data/store2.csv", "w") as f:
-    #     writer = csv.DictWriter(f, fnames)
-    #     writer.writeheader()
-    #     writer.writerows(response)
-
-
-    # store = [i for i in response["result"]]
-    
-    # print(type(store))
-    
-    # for o in store: print(str(o) + "\n")
-    
-    # print(type(response["result"]))
-    
-    # fnames = set()
-    
-    # for o in store:
-    #     for n in o:
-    #         fnames.add(n)
-
-    # with open("data/store.csv", "w") as f:
-    #     writer = csv.DictWriter(f, fnames)
-    #     writer.writeheader()
-    #     writer.writerows(store)
+    ### This is written to adjust A records and point them towards the local WAN address.
+    while True:
+        wan = network.Local.getWAN()
+        for name in records:
+            try:
+                if cf.getRecord(f"{name}.{domain}")[cf.CONTENT] != wan:
+                    cf.updateRecord("A", f"{name}.{domain}", wan)
+                    print(f"Record {name}.{domain} Updated to {wan}")
+                else:
+                    print(f"Record {name}.{domain} Correct")
+            except TypeError:
+                print(f"Record {name}.{domain} not Found")
+        time.sleep(60)
