bd74abd311876d5d7e679a85857c812a74448749
Oliver Scotten <revi99@me.com>
2022-05-19 09:19:46 +0000

    Basic Outline for Accessing Cloudflare API

--- src/network.py
+++ src/network.py
@@ -1,5 +1,4 @@
 import requests
-import CloudFlare
 
 class Local:
 
@@ -12,17 +11,41 @@ class Local:
         wan = requests.get("https://api.ipify.org/").text
         return wan
 
-class Remote(CloudFlare.CloudFlare):
+class Remote():
 
-    def __init__(self, key: str):
+    def __init__(self, token: str, domain: str):
         """Minimalized Cloudflare API connection.
 
         Args:
-            key (str): API Key
-        """
-        super().__init__(token=key)
-        zones = self.zones.get()
-        self.ID = {z['name']:z['id'] for z in zones}
+            key (str): API Token
+            domain (str): Domain
+        """   
+        req = requests.get("https://api.cloudflare.com/client/v4/zones", headers={"Authorization":f"Bearer {token}", "Content-Type": "application/json"})
+        zones = req.json()["result"]
+        
+        self._token: str = f"Bearer {token}"
+        self._zoneID: str = {z['name']:z['id'] for z in zones}[domain]
+        self.records: list[dict] = []
+        
+        ### Cleanup
+        del(req)
+        del(zones)
 
-    def getID(self) -> dict:
-        return self.ID
+    def updateLocalRecords(self) -> bool:
+        try:
+            req = requests.get(url=f"https://api.cloudflare.com/client/v4/zones/{self._zoneID}/dns_records",
+                                                    headers={"Authorization": self._token, "Content-Type": "application/json"})
+            if req.json()["errors"] == []:
+                self.records = [i for i in req.json()["result"]]
+                return True
+        except:
+            return False
+
+    def getRecord(self, name: str, type: str) -> str | int | bool:
+        pass
+    
+    def getRecords(self) -> list:
+        return self.records
+
+    def updateRecord(self, type: str, name: str, content: str, ttl: int = 1) -> bool:
+        pass
